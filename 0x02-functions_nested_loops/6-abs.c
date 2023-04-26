689ba2bbcc7fb46c80fc690d30d5328b3bd7e565
Rebecca Ekpo <r.s.ekpo1@gmail.com>
2023-04-26 12:12:16 -0700

    Abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - computes the absolute value of an integer
+ * @num: the inr=teger to be computed
+ * Return: The absolute value
+ */
+int _abs(int num)
+{
+	if (num < 0)
+		return (-num);
+	else
+		return (num);
+}
