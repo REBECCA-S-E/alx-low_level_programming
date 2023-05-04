0fbe4b8f2686d13627f4abf290a9c45b35dab576
Rebecca Ekpo <r.s.ekpo1@gmail.com>
2023-05-03 19:03:23 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: strings to be printed
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
