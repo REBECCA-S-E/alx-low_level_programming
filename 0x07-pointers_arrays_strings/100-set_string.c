b47b718e0f7eee921d6c0dc7608d6b753c3c61de
Rebecca Ekpo <r.s.ekpo1@gmail.com>
2023-05-08 22:32:54 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of pointer
+ * @s: first input
+ * @to: second input
+ * Return: Always 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
