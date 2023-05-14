7795c72a92f87511c9b9a7d26079d2f9fa896b32
Rebecca Ekpo <r.s.ekpo1@gmail.com>
2023-05-13 21:54:19 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints file name
+ * @argc: Arg count
+ * @argv: Array of strings
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
